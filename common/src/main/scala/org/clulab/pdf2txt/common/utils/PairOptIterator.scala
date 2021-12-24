0b93b48e1fafa474b8df4e85e63c64bdf4d40728
Keith Alcock <github@keithalcock.com>
2021-12-24 13:37:32 -0700

    Get PairOptIterator right

--- common/src/main/scala/org/clulab/pdf2txt/common/utils/PairOptIterator.scala
+++ common/src/main/scala/org/clulab/pdf2txt/common/utils/PairOptIterator.scala
@@ -1,18 +1,5 @@
 package org.clulab.pdf2txt.common.utils
 
-class EmptyPairOptIterator[T](soloIterable: Iterable[T]) extends Iterator[(Option[T], Option[T])] {
-  protected val soloIterator = soloIterable.iterator
-
-  def hasNext: Boolean = soloIterator.hasNext
-
-  // This should throw an exception, the same one that the soloIterator would throw.
-  def next(): (Option[T], Option[T]) = {
-    val valueOpt = Some(soloIterator.next())
-
-    (valueOpt, valueOpt)
-  }
-}
-
 // The sequence is
 // (None, Some(head))           if head = true
 // (Some(head), Some(head + 1)) if length > 1
@@ -20,31 +7,36 @@ class EmptyPairOptIterator[T](soloIterable: Iterable[T]) extends Iterator[(Optio
 // (Some(last - 1), Some(last)) if length > 1
 // (Some(last), None)           if last = true
 class PairOptIterator[T](soloIterable: Iterable[T], head: Boolean = true, last: Boolean = true) extends Iterator[(Option[T], Option[T])] {
-  require(soloIterable.nonEmpty)
-
   protected val soloIterator = soloIterable.iterator
   protected var isHead = true
   protected var isLast = false
-  protected var prevOpt: Option[T] = None
+  protected var prevOpt: Option[T] =
+      if (soloIterator.hasNext) Some(soloIterator.next())
+      else None
 
   def hasNext: Boolean =
-      isHead && head && soloIterator.hasNext ||
+      head && isHead && prevOpt.isDefined ||
       prevOpt.isDefined && soloIterator.hasNext ||
-      isLast && last && prevOpt.isDefined
+      last && (isLast || prevOpt.isDefined) // for soloist
 
   def next(): (Option[T], Option[T]) = {
     val result =
-        if (isHead && head && soloIterator.hasNext) {
+        if (head && isHead && prevOpt.isDefined) {
+          val result = (None, prevOpt)
           isHead = false
-          (None, Some(soloIterator.next()))
+          isLast = !soloIterator.hasNext
+          result
         }
-        else if (isLast && last && prevOpt.isDefined) {
-          isLast = false
-          (prevOpt, None)
+        else if (prevOpt.isDefined && soloIterator.hasNext) {
+          val result = (prevOpt, Some(soloIterator.next()))
+          isHead = false
+          isLast = !soloIterator.hasNext
+          result
         }
         else {
-          val result = (prevOpt, Some(soloIterator.next()))
-          isLast = !soloIterator.hasNext
+          val result = (prevOpt, None)
+          isHead = false
+          isLast = false
           result
         }
 
@@ -55,7 +47,6 @@ class PairOptIterator[T](soloIterable: Iterable[T], head: Boolean = true, last: 
 
 object PairOptIterator {
   def apply[T](soloIterable: Iterable[T], head: Boolean = true, last: Boolean = true): Iterator[(Option[T], Option[T])] = {
-    if (soloIterable.isEmpty) new EmptyPairOptIterator(soloIterable)
-    else new PairOptIterator(soloIterable, head, last)
+    new PairOptIterator(soloIterable, head, last)
   }
 }
--- common/src/test/scala/org/clulab/pdf2txt/common/utils/TestPairOptIterator.scala
+++ common/src/test/scala/org/clulab/pdf2txt/common/utils/TestPairOptIterator.scala
@@ -8,9 +8,9 @@ class TestPairOptIterator extends Test {
     val iterable = Seq.empty[Int]
 
     PairOptIterator(iterable, false, false) shouldBe empty
-    PairOptIterator(iterable, false, true)  shouldBe empty
-    PairOptIterator(iterable, true,  false) shouldBe empty
-    PairOptIterator(iterable, true,  true)  shouldBe empty
+    PairOptIterator(iterable, true , false) shouldBe empty
+    PairOptIterator(iterable, false, true ) shouldBe empty
+    PairOptIterator(iterable, true , true ) shouldBe empty
   }
 
   behavior of "short PairOptIterator"
@@ -19,18 +19,26 @@ class TestPairOptIterator extends Test {
     val iterable = Seq(0)
 
     PairOptIterator(iterable, false, false) should have length 0
-    PairOptIterator(iterable, false, true)  should have length 1
-    PairOptIterator(iterable, true,  false) should have length 1
-    PairOptIterator(iterable, true,  true)  should have length 2
+    PairOptIterator(iterable, true , false) should have length 1
+    PairOptIterator(iterable, false, true ) should have length 1
+    PairOptIterator(iterable, true , true ) should have length 2
   }
 
   it should "have the correct content" in {
     val iterable = Seq(0)
 
-    PairOptIterator(iterable, false, false) should have length 0
-    PairOptIterator(iterable, false, true)  should have length 1
-    PairOptIterator(iterable, true,  false) should have length 1
-    PairOptIterator(iterable, true,  true)  should have length 2
+    PairOptIterator(iterable, false, false).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+    )
+    PairOptIterator(iterable, true , false).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (None, Some(0))
+    )
+    PairOptIterator(iterable, false, true ).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (Some(0), None)
+    )
+    PairOptIterator(iterable, true , true ).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (None,    Some(0)),
+      (Some(0), None)
+    )
   }
 
   behavior of "longer PairOptIterator"
@@ -39,18 +47,30 @@ class TestPairOptIterator extends Test {
     val iterable = Seq(1, 2)
 
     PairOptIterator(iterable, false, false) should have length 1
-    PairOptIterator(iterable, false, true)  should have length 2
-    PairOptIterator(iterable, true,  false) should have length 2
-    PairOptIterator(iterable, true,  true)  should have length 3
+    PairOptIterator(iterable, true , false) should have length 2
+    PairOptIterator(iterable, false, true ) should have length 2
+    PairOptIterator(iterable, true , true ) should have length 3
   }
 
   it should "have the correct content" in {
     val iterable = Seq(1, 2)
 
-    PairOptIterator(iterable, false, false) should have length 1
-    PairOptIterator(iterable, false, true)  should have length 2
-    PairOptIterator(iterable, true,  false) should have length 2
-    PairOptIterator(iterable, true,  true)  should have length 3
+    PairOptIterator(iterable, false, false).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (Some(1), Some(2))
+    )
+    PairOptIterator(iterable, true , false).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (None,    Some(1)),
+      (Some(1), Some(2))
+    )
+    PairOptIterator(iterable, false, true ).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (Some(1), Some(2)),
+      (Some(2), None)
+    )
+    PairOptIterator(iterable, true , true ).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (None,    Some(1)),
+      (Some(1), Some(2)),
+      (Some(2), None)
+    )
   }
 
   behavior of "long PairOptIterator"
@@ -58,6 +78,11 @@ class TestPairOptIterator extends Test {
   it should "have the correct content" in {
     val iterable = Seq(1, 2, 3)
 
-    PairOptIterator(iterable, false, false) should have length 1
+    PairOptIterator(iterable, true, true).map(value => value).toSeq should contain theSameElementsInOrderAs Seq(
+      (None,    Some(1)),
+      (Some(1), Some(2)),
+      (Some(2), Some(3)),
+      (Some(3), None)
+    )
   }
 }
