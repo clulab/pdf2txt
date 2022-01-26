bc0ee88c809d91e9c9f5776d0b291ef2d5e7f61d
Keith Alcock <github@keithalcock.com>
2022-01-26 16:26:50 -0700

    Stop at hyphens in left and right

--- src/main/scala/org/clulab/pdf2txt/languageModel/DictionaryLanguageModel.scala
+++ src/main/scala/org/clulab/pdf2txt/languageModel/DictionaryLanguageModel.scala
@@ -1,6 +1,7 @@
 package org.clulab.pdf2txt.languageModel
 
 import org.clulab.embeddings.CompactWordEmbeddingMap.{BuildType, mkMapFromText}
+import org.clulab.pdf2txt.common.utils.StringUtils
 import org.clulab.utils.ClassLoaderObjectInputStream
 import org.clulab.utils.Closer.AutoCloser
 
@@ -17,8 +18,10 @@ class DictionaryLanguageModel extends LanguageModel {
   }
 
   override def shouldJoin(left: String, right: String, prevWords: Seq[String]): Boolean = {
-    val contains = words.contains(left + right)
-    val betweenDigits = left.last.isDigit && right.head.isDigit
+    val leftAfterHyphen = StringUtils.afterLast(left, StringUtils.HYPHEN, all = true)
+    val rightBeforeHyphen = StringUtils.beforeFirst(right, StringUtils.HYPHEN, all = true)
+    val contains = words.contains(leftAfterHyphen + rightBeforeHyphen)
+    val betweenDigits = leftAfterHyphen.last.isDigit && rightBeforeHyphen.head.isDigit
 
     contains && !betweenDigits
   }
