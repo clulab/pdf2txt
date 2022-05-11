73a67d7627a51b450ec0ca4358321e6c374dd7a0
Keith Alcock <github@keithalcock.com>
2022-05-11 15:23:12 -0700

    Tidy TextRange

--- common/src/main/scala/org/clulab/pdf2txt/common/utils/TextRange.scala
+++ common/src/main/scala/org/clulab/pdf2txt/common/utils/TextRange.scala
@@ -5,10 +5,24 @@ import org.clulab.pdf2txt.common.utils.StringUtils._
 import scala.collection.mutable
 import scala.util.matching.Regex
 
-class TextRange(val text: String, val range: Range) extends IndexedSeq[Char] {
+class TextRange(val text: String, val range: Range) extends IndexedSeq[Char] with CharSequence {
 
+  // Copy constructor
   def this(textRange: TextRange) = this(textRange.text, textRange.range)
 
+  // IndexedSeq[Char] interface
+  def length: Int = range.length
+
+  override def apply(index: Int): Char =
+    if (0 <= index && index < length) text(start + index)
+    else throw new IndexOutOfBoundsException(s"$index is not within interval [0, $length)!")
+
+  // CharSequence interface
+  override def charAt(index: Int): Char = this(index)
+
+  override def subSequence(start: Int, end: Int): CharSequence = subRange(this.start + start, this.start + end)
+
+  // Other methods
   override def toString: String = text.substring(range)
 
   def toString(stringBuilder: StringBuilder): StringBuilder = {
@@ -16,17 +30,13 @@ class TextRange(val text: String, val range: Range) extends IndexedSeq[Char] {
     stringBuilder
   }
 
-  override def isEmpty: Boolean = range.isEmpty
-
+  // Getters
   def start: Int = range.start
 
   def end: Int = range.end
 
-  def findAll(regex: Regex): IndexedSeq[TextRange] = {
-    regex.findAllMatchIn(toString).map { found =>
-      subRange(found.start, found.end) + range.start
-    }.toIndexedSeq
-  }
+  // Empty
+  override def isEmpty: Boolean = range.isEmpty
 
   def emptyStart: TextRange = emptyRange(start)
 
@@ -34,11 +44,69 @@ class TextRange(val text: String, val range: Range) extends IndexedSeq[Char] {
 
   def emptyRange(pos: Int): TextRange = TextRange(text, Range(pos, pos))
 
+  def trimmedIsEmpty: Boolean = findFirstTrimmed.isEmpty
+
+  // Slice.  Be careful.  Coordinates start with the text, not existing range.
   def subRange(range: Range): TextRange = TextRange(text, range)
 
   def subRange(pos: Int): TextRange = subRange(Range(pos, pos + 1))
 
   def subRange(start: Int, end: Int): TextRange = TextRange(text, Range(start, end))
+
+  // Before
+  def andBefore(textRange: TextRange): TextRange = before(textRange.end)
+
+  def before(pos: Int): TextRange = subRange(start, pos)
+
+  def before(textRange: TextRange): TextRange = before(textRange.start)
+
+  // After
+  def after(pos: Int): TextRange = subRange(pos, end)
+
+  def after(textRange: TextRange): TextRange = after(textRange.end)
+
+  def andAfter(textRange: TextRange): TextRange = after(textRange.start)
+
+  // First
+  def firstChar: Char = text(start) // assuming nonEmpty
+
+  def findFirstTrimmed: TextRange = findFirst { char: Char => char > ' ' }
+
+  def withoutFirst: TextRange = if (isEmpty) this else subRange(start + 1, end)
+
+  // Last
+  def lastChar: Char = text(end - 1) // assuming nonEmpty
+
+  def findLastTrimmed: TextRange = findLast { char: Char => char > ' ' }
+
+  def withoutLast: TextRange = if (isEmpty) this else subRange(start, end - 1)
+
+  // Miscellaneous
+  def all: TextRange = this
+
+  def +(pos: Int): TextRange = subRange(pos + start, pos + end)
+
+  // Search and replace
+  def matches(char: Char): Boolean = length == 1 && head == char
+
+  def matches(string: String): Boolean =
+      string.length == length &&
+      string.indices.forall { index => string(index) == this(index) }
+
+  def findFirst(f: Char => Boolean): TextRange = range
+      .find { pos => f(text(pos)) }
+      .map(subRange)
+      .getOrElse(emptyEnd)
+
+  def findLast(f: Char => Boolean): TextRange = range.reverse
+      .find { pos => f(text(pos)) }
+      .map(subRange)
+      .getOrElse(emptyStart)
+
+  def findAll(regex: Regex): IndexedSeq[TextRange] = regex
+      .findAllMatchIn(this)
+      .map { found => subRange(found.start, found.end) + range.start }
+      .toIndexedSeq
 
   def removeAll(textRanges: IndexedSeq[TextRange]): IndexedSeq[TextRange] = {
     // The ranges should be pre-sorted and be based on the same text.
@@ -76,65 +144,6 @@ class TextRange(val text: String, val range: Range) extends IndexedSeq[Char] {
       preTextRanges ++ interTextRanges ++ postTextRanges
     }
   }
-
-  def matches(char: Char): Boolean = length == 1 && head == char
-
-  def matches(string: String): Boolean = {
-    val offset = start
-
-    length == string.length &&
-        string.indices.forall { index => string(index) == text(offset + index) }
-  }
-
-  def andBefore(textRange: TextRange): TextRange = before(textRange.end)
-
-  def before(pos: Int): TextRange = subRange(start, pos)
-
-  def before(textRange: TextRange): TextRange = before(textRange.start)
-
-  def after(pos: Int): TextRange = subRange(pos, end)
-
-  def after(textRange: TextRange): TextRange = after(textRange.end)
-
-  def andAfter(textRange: TextRange): TextRange = after(textRange.start)
-
-  def all: TextRange = this
-
-  def length: Int = range.length
-
-  // Is dropRight(1)
-  // dropLast
-  def withoutLast: TextRange = if (isEmpty) this else subRange(start, end - 1)
-
-  // take()
-  // drop()
-  def withoutHead: TextRange = if (isEmpty) this else subRange(1, end)
-
-  def +(pos: Int): TextRange = subRange(pos + start, pos + end)
-
-  override def apply(index: Int): Char =
-      if (0 <= index && index < length) text(start + index)
-      else throw new IndexOutOfBoundsException(s"$index is not within interval [0, $length)!")
-
-  def findFirst(f: Char => Boolean): TextRange = range
-      .find { pos => f(text(pos)) }
-      .map(subRange)
-      .getOrElse(emptyEnd)
-
-  def findLast(f: Char => Boolean): TextRange = range.reverse
-      .find { pos => f(text(pos)) }
-      .map(subRange)
-      .getOrElse(emptyStart)
-
-  def findFirstTrimmed: TextRange = findFirst { char: Char => char > ' ' }
-
-  def findLastTrimmed: TextRange = findLast { char: Char => char > ' ' }
-
-  def firstChar: Char = text(start) // assuming nonEmpty
-
-  def lastChar: Char = text(end - 1) // assuming nonEmpty
-
-  def trimmedIsEmpty: Boolean = findFirstTrimmed.isEmpty
 }
 
 object TextRange {
@@ -170,12 +179,4 @@ object TextRanges {
   def apply(textRange: TextRange): TextRanges = new TextRanges() += textRange
 
   def apply(textRanges: Seq[TextRange]): TextRanges = new TextRanges() ++= textRanges
-
-  def test: Unit = {
-    val textRange = TextRange("hello")
-
-    textRange.foreach { char =>
-      println(char)
-    }
-  }
 }
