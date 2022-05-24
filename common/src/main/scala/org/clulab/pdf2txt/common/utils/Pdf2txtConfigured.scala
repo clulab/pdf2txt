4e98b492ee65d3e4bc60f0753901d184b3c3671e
Keith Alcock <github@keithalcock.com>
2022-05-24 16:08:04 -0700

    Allow user.home

--- common/src/main/scala/org/clulab/pdf2txt/common/utils/Pdf2txtConfigured.scala
+++ common/src/main/scala/org/clulab/pdf2txt/common/utils/Pdf2txtConfigured.scala
@@ -1,11 +1,14 @@
 package org.clulab.pdf2txt.common.utils
 
-import com.typesafe.config.Config
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
+
+import scala.sys.SystemProperties
 
 trait Pdf2txtConfigured extends Configured {
   // This line doesn't work if there is a leading / in the resource name.  I tried.
-  lazy val config = ConfigFactory.parseResourcesAnySyntax("Pdf2txt").getConfig("Pdf2txt")
+  lazy val config = ConfigFactory.parseResourcesAnySyntax("Pdf2txt")
+      .resolveWith(ConfigFactory.systemProperties)
+      .getConfig("Pdf2txt")
 
   override def getConfig: Config = config
 }
