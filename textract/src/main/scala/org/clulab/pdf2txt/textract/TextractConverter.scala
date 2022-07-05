b6118498f8ccfb893a60d0a4877e78e4f817c7fb
Keith Alcock <github@keithalcock.com>
2022-07-04 18:30:28 -0700

    Get two-pager to work

--- textract/src/main/scala/org/clulab/pdf2txt/textract/TextractConverter.scala
+++ textract/src/main/scala/org/clulab/pdf2txt/textract/TextractConverter.scala
@@ -7,12 +7,13 @@ import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.profiles.ProfileFile
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.textract.TextractClient
-import software.amazon.awssdk.services.textract.model.{BlockType, DetectDocumentTextRequest, Document}
+import software.amazon.awssdk.services.textract.model.{Block, BlockType, DetectDocumentTextRequest, Document, DocumentLocation, FeatureType, GetDocumentTextDetectionRequest, S3Object, StartDocumentAnalysisRequest, StartDocumentTextDetectionRequest}
 
 import java.io.{File, FileInputStream}
 import java.util.concurrent.atomic.AtomicBoolean
 import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 class TextractConverter(textractSettings: TextractSettings = TextractConverter.defaultSettings) extends PdfConverter {
   val isOpen = new AtomicBoolean(false)
@@ -40,18 +41,7 @@ class TextractConverter(textractSettings: TextractSettings = TextractConverter.d
 
   override def close(): Unit = if (isOpen.getAndSet(false)) textractClient.close()
 
-  override def convert(pdfFile: File): String = {
-    val sdkBytes = new FileInputStream(pdfFile).autoClose { inputStream =>
-      SdkBytes.fromInputStream(inputStream)
-    }
-    val document = Document.builder()
-        .bytes(sdkBytes)
-        .build()
-    val detectDocumentTextRequest = DetectDocumentTextRequest.builder()
-        .document(document)
-        .build()
-    val detectDocumentTextResponse = textractClient.detectDocumentText(detectDocumentTextRequest)
-    val blocks = detectDocumentTextResponse.blocks.asScala
+  def convertBlocks(blocks: mutable.Seq[Block]): String = {
     // It doesn't need to be thread-safe, so the faster StringBuilder is preferred.
     val stringBuilder = new StringBuilder()
 
@@ -65,11 +55,74 @@ class TextractConverter(textractSettings: TextractSettings = TextractConverter.d
           stringBuilder.append(block.text)
           stringBuilder.append("\n")
         case BlockType.WORD =>
-          // These appear to be duplicated in the lines.
+        // These appear to be duplicated in the lines.
         case _ =>
+          println("Unknown blocktype")
       }
     }
     stringBuilder.toString
+  }
+
+  def convertSinglePage(pdfFile: File): String = {
+    val sdkBytes = new FileInputStream(pdfFile).autoClose { inputStream =>
+      SdkBytes.fromInputStream(inputStream)
+    }
+    val document = Document.builder()
+        .bytes(sdkBytes)
+        .build()
+    val detectDocumentTextRequest = DetectDocumentTextRequest.builder()
+        .document(document)
+        .build()
+    val detectDocumentTextResponse = textractClient.detectDocumentText(detectDocumentTextRequest)
+    val blocks = detectDocumentTextResponse.blocks.asScala
+
+    convertBlocks(blocks)
+  }
+
+  def convertMultiplePages(pdfFile: File): String = {
+    val documentLocation = DocumentLocation.builder()
+      .s3Object(
+        S3Object.builder()
+          .bucket("awspdf-pdf")
+          .name(pdfFile.getName)
+          .build()
+      )
+      .build()
+    val startDocumentTextDetectionRequest = StartDocumentTextDetectionRequest.builder()
+        .documentLocation(documentLocation)
+        .build()
+
+    val startDocumentTextDetectionRequestResponse = textractClient.startDocumentTextDetection(startDocumentTextDetectionRequest)
+    val jobId = startDocumentTextDetectionRequestResponse.jobId()
+
+    // wait until finished?, get first one that not IN_PROGRESS
+    // while waitWhileInProgress
+    var done = false
+
+    while (!done) {
+      Thread.sleep(1000)
+      val getDocumentTextDetectionRequest = GetDocumentTextDetectionRequest.builder()
+        .jobId(jobId)
+        .build()
+      val getDocumentTextDetectionResponse = textractClient.getDocumentTextDetection(getDocumentTextDetectionRequest)
+
+      if (getDocumentTextDetectionResponse.jobStatusAsString == "IN_PROGRESS")
+        done = false
+      else done = true
+    }
+
+    val getDocumentTextDetectionRequest = GetDocumentTextDetectionRequest.builder()
+      .jobId(jobId)
+      .build()
+    val getDocumentTextDetectionResponse = textractClient.getDocumentTextDetection(getDocumentTextDetectionRequest)
+    val blocks = getDocumentTextDetectionResponse.blocks.asScala
+
+    convertBlocks(blocks)
+  }
+
+  override def convert(pdfFile: File): String = {
+    // convertSinglePage(pdfFile)
+    convertMultiplePages(pdfFile)
   }
 }
 
