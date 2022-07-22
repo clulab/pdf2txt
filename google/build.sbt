43fa9fc938e678f35911eebbfa0806ddcf3529cc
Keith Alcock <github@keithalcock.com>
2022-07-21 22:47:09 -0700

    Combine multiple pages

--- google/build.sbt
+++ google/build.sbt
@@ -7,10 +7,14 @@ resolvers ++= Seq(
 )
 
 libraryDependencies ++= {
+  val json4sVersion = "3.5.2"
+
   Seq(
+    "com.google.apis"  % "google-api-services-vision"      % "v1-rev451-1.25.0",
     "com.google.cloud" % "google-cloud-storage"            % "2.10.0",
     "com.google.cloud" % "google-cloud-vision"             % "3.0.1",
-    "com.google.apis"  % "google-api-services-vision"      % "v1-rev451-1.25.0",
-    "com.google.auth"  % "google-auth-library-oauth2-http" % "1.8.1"
+    "com.google.auth"  % "google-auth-library-oauth2-http" % "1.8.1",
+    "org.json4s"      %% "json4s-core"                     % json4sVersion,
+    "org.json4s"      %% "json4s-jackson"                  % json4sVersion
   )
 }
--- google/src/main/scala/org/clulab/pdf2txt/google/GoogleConverter.scala
+++ google/src/main/scala/org/clulab/pdf2txt/google/GoogleConverter.scala
@@ -1,54 +1,70 @@
 package org.clulab.pdf2txt.google
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.gax.core.CredentialsProvider
-import com.google.auth.http.HttpCredentialsAdapter
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.api.services.vision.v1.Vision
 import com.google.api.services.vision.v1.VisionScopes
 import com.google.auth.Credentials
+import com.google.cloud.storage.Bucket
 import com.google.cloud.storage.Storage.BlobListOption
-import com.google.cloud.storage.{Storage, StorageOptions}
-import com.google.cloud.vision.v1.{AnnotateFileResponse, AsyncAnnotateFileRequest, Feature, GcsDestination, GcsSource, ImageAnnotatorClient, ImageAnnotatorSettings, InputConfig, OutputConfig}
-import com.google.protobuf.util.JsonFormat
+import com.google.cloud.storage.StorageOptions
+import com.google.cloud.vision.v1.{AsyncAnnotateFileRequest, Feature, GcsDestination, GcsSource, ImageAnnotatorClient, ImageAnnotatorSettings, InputConfig, OutputConfig}
 import org.clulab.pdf2txt.common.pdf.PdfConverter
 import org.clulab.pdf2txt.common.utils.Closer.AutoCloser
 import org.clulab.pdf2txt.common.utils.FileEditor
+import org.clulab.pdf2txt.common.utils.FileUtils
+import org.json4s.{JArray, JInt, JString}
+import org.json4s.jackson.JsonMethods
 
 import java.io.{File, FileInputStream}
 import java.util.ArrayList
-import java.util.regex.Pattern
+import java.util.concurrent.atomic.AtomicBoolean
 import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
 
-class GoogleConverter(googleSettings: GoogleSettings = GoogleConverter.defaultSettings) extends PdfConverter {
+class GoogleConverter(googleSettings: GoogleSettings = GoogleConverter.defaultSettings) extends PdfConverter with CredentialsProvider {
   // See https://github.com/googleapis/java-vision/blob/HEAD/samples/snippets/src/main/java/com/example/vision/Detect.java.
-
-  // make it a this
-  class MyCredentialsProvider(googleCredentials: GoogleCredentials) extends CredentialsProvider {
-    override def getCredentials: Credentials = googleCredentials
-  }
-
-  override def convert(pdfFile: File): String = {
+  val isOpen = new AtomicBoolean(false)
+  lazy val googleCredentials: GoogleCredentials = {
     val inputStream = new FileInputStream(new File(googleSettings.credentials))
     val googleCredentials = GoogleCredentials.fromStream(inputStream).createScoped(VisionScopes.all())
-    val credentialsProvider = new MyCredentialsProvider(googleCredentials)
-    val imageAnnotatorSettings = ImageAnnotatorSettings.newBuilder().setCredentialsProvider(credentialsProvider).build()
 
-    // TODO: Save the json files locally like the textract ones
-    val client: ImageAnnotatorClient = ImageAnnotatorClient.create(imageAnnotatorSettings)
-//    val storageOptions = StorageOptions.getDefaultInstance().getService()
+    googleCredentials
+  }
+  lazy val imageAnnotatorClient: ImageAnnotatorClient = {
+    val imageAnnotatorSettings = ImageAnnotatorSettings.newBuilder().setCredentialsProvider(this).build()
+
+    isOpen.set(true)
+    ImageAnnotatorClient.create(imageAnnotatorSettings)
+  }
+  lazy val bucket: Bucket = {
     val storageOptions = StorageOptions.newBuilder().setCredentials(googleCredentials).build()
-    val storage = storageOptions.getService()
-    val stringBuilder = new StringBuilder()
-    val maxPages = 10
+    val storage = storageOptions.getService
+    val bucket = storage.get(googleSettings.bucket)
 
+    bucket
+  }
+
+  def getCredentials: Credentials = googleCredentials
+
+  override def close(): Unit = if (isOpen.getAndSet(false)) imageAnnotatorClient.close()
+
+  def getText(jResponses: JArray): String = {
+    val texts = jResponses.arr.map { response =>
+      val text = (response \ "fullTextAnnotation" \ "text").asInstanceOf[JString].values
+
+      text
+    }
+
+    texts.mkString("\n")
+  }
+
+  def convertPdf(pdfFile: File, jsonFile: File): String = {
     val pdfFileName = pdfFile.getName
+    // Add a . to separate the prefix from the suffix.
     val jsonFilePrefix = FileEditor(pdfFile).setExt(inputExtension + ".").get.getName
-    val bucket = "pdf2txt_pdfs"
-    val gcsSourcePath = s"gs://$bucket/$pdfFileName"
-    val gcsDestinationPath = s"gs://$bucket/$jsonFilePrefix"
+    val bucketName = googleSettings.bucket
+    val gcsSourcePath = s"gs://$bucketName/$pdfFileName"
+    val gcsDestinationPath = s"gs://$bucketName/$jsonFilePrefix"
 
     val gcsSource = GcsSource.newBuilder().setUri(gcsSourcePath).build()
     val inputConfig = InputConfig.newBuilder()
@@ -57,7 +73,7 @@ class GoogleConverter(googleSettings: GoogleSettings = GoogleConverter.defaultSe
         .build()
     val gcsDestination = GcsDestination.newBuilder().setUri(gcsDestinationPath).build()
     val outputConfig = OutputConfig.newBuilder()
-        .setBatchSize(maxPages)
+        .setBatchSize(GoogleConverter.batchSize)
         .setGcsDestination(gcsDestination)
         .build()
     val feature = Feature.newBuilder()
@@ -68,65 +84,69 @@ class GoogleConverter(googleSettings: GoogleSettings = GoogleConverter.defaultSe
         .setInputConfig(inputConfig)
         .setOutputConfig(outputConfig)
         .build()
+    val requests = {
+      val requests = new ArrayList[AsyncAnnotateFileRequest]()
 
-    client.autoClose { client =>
-      val requests = {
-        val requests = new ArrayList[AsyncAnnotateFileRequest]()
-
-        requests.add(request)
-        requests
-      }
-      val response = client.asyncBatchAnnotateFilesAsync(requests)
-
-      // Wait for the request to finish. (The result is not used, since the API saves the result to
-      // the specified location on GCS.)
-      response.get().getResponsesList()
-
-      // Get the destination location from the gcsDestinationPath
-      val pattern = Pattern.compile("gs://([^/]+)/(.+)")
-      val matcher = pattern.matcher(gcsDestinationPath)
-
-      // This can all be done ahead of time?
-      if (matcher.find()) {
-        val bucketName = matcher.group(1)
-        val prefix = matcher.group(2)
-        val bucket = storage.get(bucketName)
-        val pageList = bucket.list(BlobListOption.prefix(prefix))
-
-        pageList.iterateAll.asScala.foreach { blob =>
-          println(blob.getName)
-
-          val jsonContents = new String(blob.getContent()) // doesn't this need a charset?
-          val builder = AnnotateFileResponse.newBuilder()
-          JsonFormat.parser().merge(jsonContents, builder)
-
-          val annotateFileResponse = builder.build()
-          val annotateImageResponse = annotateFileResponse.getResponses(0) // get whole list instead?
-          val text = annotateImageResponse.getFullTextAnnotation().getText()
-          // each response has context/pageNumber
-
-          // find the page number, make map of page number to json
-          // combine them into an array after sorting
-          // blob.delete()
-          println(text)
-          stringBuilder.append(text)
-        }
-      }
+      requests.add(request)
+      requests
     }
 
-    stringBuilder.toString
+    // Wait for the request to finish. (The result is not used, since the
+    // API saves the result to the specified location on GCS.)
+    imageAnnotatorClient.asyncBatchAnnotateFilesAsync(requests).get().getResponsesList
+
+    val pageList = bucket.list(BlobListOption.prefix(jsonFilePrefix))
+    val pageAndResponseTuples = pageList.iterateAll.asScala.flatMap { blob =>
+      val json = new String(blob.getContent()) // Doesn't this need a charset?
+      val jValue = JsonMethods.parse(json)
+      val jArray = (jValue \ "responses").asInstanceOf[JArray]
+      val pageNumberAndResponsTuples = jArray.arr.map { response =>
+        val pageNumber = (response \ "context" \ "pageNumber").asInstanceOf[JInt].values.toInt
+
+        pageNumber -> response
+      }
+
+      pageNumberAndResponsTuples
+    }.toVector
+    val responses = pageAndResponseTuples.sortBy(_._1).map(_._2)
+    val jResponses = JArray(responses.toList)
+    val text = getText(jResponses)
+
+    FileUtils.printWriterFromFile(jsonFile).autoClose { printWriter =>
+      val json = JsonMethods.pretty(jResponses)
+
+      printWriter.println(json)
+    }
+    text
+  }
+
+  def convertJson(jsonFile: File): String = {
+    val json = FileUtils.getTextFromFile(jsonFile)
+    val jResponses = JsonMethods.parse(json).asInstanceOf[JArray]
+    val text = getText(jResponses)
+
+    text
+  }
+
+  override def convert(pdfFile: File): String = {
+    val jsonFile = new FileEditor(pdfFile).setExt(".json").get
+
+    if (jsonFile.exists) convertJson(jsonFile)
+    else convertPdf(pdfFile, jsonFile)
   }
 }
 
-case class GoogleSettings(@BeanProperty var credentials: String) {
-  def this() = this("")
+case class GoogleSettings(@BeanProperty var credentials: String, @BeanProperty var bucket: String) {
+  def this() = this("", "")
 }
 
 object GoogleConverter {
-  val defaultCredentials = {
+  val batchSize = 10
+  val defaultCredentials: String = {
     val userHome = System.getProperty("user.home")
     s"$userHome/.pdf2txt/google-credentials.json"
   }
   val defaultApplication = "pdf2txt"
-  val defaultSettings = GoogleSettings(defaultCredentials)
+  val defaultBucket = ""
+  val defaultSettings: GoogleSettings = GoogleSettings(defaultCredentials, defaultBucket)
 }
